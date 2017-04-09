abab21456e963570c69ca4759ab34298ad0374c1
Calvin Kwong <ckwong93@gmail.com>
2017-04-08 17:15:30 -0700

    complete contact footer

--- js/application.js
+++ js/application.js
@@ -1,19 +1,5 @@
-$(document).ready(function () {
-  map();
-});
-
-function map() {
-  var mapProp = {
-    center:new google.maps.LatLng(37.434499, -121.902226),
-    zoom:20,
-    mapTypeId:google.maps.MapTypeId.ROADMAP
-  };
-  var myLatLng= {lat: 37.434499,lng: -121.902226}
-  var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-  var marker = new google.maps.Marker({
-          position: myLatLng,
-          map: map,
-          title: "Koong's"
-        });
-
-}
+// $('.map-container')
+// 	.click(function(){
+// 			$(this).find('iframe').addClass('clicked')})
+// 	.mouseleave(function(){
+// 			$(this).find('iframe').removeClass('clicked')});
